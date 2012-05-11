56e378129a6c271c2fb4a930efca6e464232abb4
Achim Nierbeck <bcanhome@googlemail.com>
2012-05-11 20:59:09 +0200

    Fixed dependencies to OSGi Bundles for GWT, added some License Headers

--- pax-vaadin-service/src/main/java/org/ops4j/pax/vaadin/internal/servlet/VaadinResourceServlet.java
+++ pax-vaadin-service/src/main/java/org/ops4j/pax/vaadin/internal/servlet/VaadinResourceServlet.java
@@ -1,3 +1,20 @@
+/*
+ * Copyright 2012 Achim Nierbeck.
+ *
+ * Licensed  under the  Apache License,  Version 2.0  (the "License");
+ * you may not use  this file  except in  compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed  under the  License is distributed on an "AS IS" BASIS,
+ * WITHOUT  WARRANTIES OR CONDITIONS  OF ANY KIND, either  express  or
+ * implied.
+ *
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.ops4j.pax.vaadin.internal.servlet;
 
 import java.io.IOException;
@@ -18,20 +35,20 @@ import org.osgi.framework.Bundle;
 public class VaadinResourceServlet extends HttpServlet implements VaadinResourceService {
 
 	/**
-	 * 
+	 *
 	 */
 	private static final long serialVersionUID = 1L;
-	
+
 	public static final String _VAADIN = "/VAADIN";
-	
+
 	private final Bundle vaadin;
-	
+
 	private final List<Bundle> resourceBundles = new ArrayList<Bundle>();
-	
+
 	public VaadinResourceServlet(Bundle vaadin) {
 		this.vaadin = vaadin;
 	}
-	
+
 	@Override
 	protected void doGet(HttpServletRequest req,
 			HttpServletResponse resp) throws ServletException,
@@ -40,11 +57,11 @@ public class VaadinResourceServlet extends HttpServlet implements VaadinResource
 		String resourcePath = _VAADIN + path;
 
 		URL resourceUrl = vaadin.getResource(resourcePath);
-		
+
 		if (null == resourceUrl) {
 			resourceUrl = loadFromResources(resourcePath);
 		}
-		
+
 		if (null == resourceUrl) {
 			resp.sendError(HttpServletResponse.SC_NOT_FOUND);
 			return;
